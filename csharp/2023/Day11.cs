576f7b5b1499b8e953993df4b5d6d3d5bbc3ae11
Jeremy Barton <bartonjs@gmail.com>
2023-12-10 21:19:46 -0800

    Day 11 refactored

--- csharp/2023/Day11.cs
+++ csharp/2023/Day11.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Data;
 using System.Linq;
 using AdventOfCode.Util;
 
@@ -10,76 +9,18 @@ namespace AdventOfCode2023
     {
         internal static void Problem1()
         {
-            long ret = 0;
-            HashSet<int> emptyCols = null;
-
-            int origWidth = -1;
-            List<Point> galaxies = new List<Point>();
-            int y = 0;
-
-            foreach (string s in Data.Enumerate())
-            {
-                int galaxyLoc = s.IndexOf('#');
-
-                if (emptyCols is null)
-                {
-                    emptyCols = new HashSet<int>(Enumerable.Range(0, s.Length));
-                    origWidth = s.Length;
-                }
-
-                if (galaxyLoc == -1)
-                {
-                    y++;
-                }
-                else
-                {
-                    while (galaxyLoc != -1)
-                    {
-                        emptyCols.Remove(galaxyLoc);
-                        galaxies.Add(new Point(galaxyLoc, y));
-
-                        galaxyLoc = s.IndexOf('#', galaxyLoc + 1);
-                    }
-                }
-
-                y++;
-            }
-
-            for (int i = origWidth - 1; i >= 0; i--)
-            {
-                if (emptyCols.Contains(i))
-                {
-                    for (int galaxyId = 0; galaxyId < galaxies.Count; galaxyId++)
-                    {
-                        if (galaxies[galaxyId].X >= i)
-                        {
-                            galaxies[galaxyId] = galaxies[galaxyId].East();
-                        }
-                    }
-                }
-            }
-
-            for (int fromId = 0; fromId < galaxies.Count; fromId++)
-            {
-                Point fromPoint = galaxies[fromId];
-
-                for (int toId = fromId + 1; toId < galaxies.Count; toId++)
-                {
-                    Point toPoint = galaxies[toId];
-
-                    ret += fromPoint.ManhattanDistance(toPoint);
-                }
-            }
-
-            Console.WriteLine(ret);
+            MetaProblem(1);
         }
 
         internal static void Problem2()
         {
+            MetaProblem(1_000_000 - 1);
+        }
+
+        private static void MetaProblem(long expansion)
+        {
             long ret = 0;
             HashSet<int> emptyCols = null;
-
-            const long Expansion = 999_999;
 
             int origWidth = -1;
             List<LongPoint> galaxies = new List<LongPoint>();
@@ -97,7 +38,7 @@ namespace AdventOfCode2023
 
                 if (galaxyLoc == -1)
                 {
-                    y += Expansion;
+                    y += expansion;
                 }
                 else
                 {
@@ -123,7 +64,7 @@ namespace AdventOfCode2023
 
                         if (point.X >= i)
                         {
-                            galaxies[galaxyId] = new LongPoint(point.X + Expansion, point.Y);
+                            galaxies[galaxyId] = new LongPoint(point.X + expansion, point.Y);
                         }
                     }
                 }
