01eee1cd4892d28f81a590bd5275969e84ae3b1e
Jeremy Barton <bartonjs@gmail.com>
2024-11-30 23:11:13 -0800

    Day 1

--- csharp/2024/Day01.cs
+++ csharp/2024/Day01.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using AdventOfCode.Util;
 
 namespace AdventOfCode2024
@@ -8,16 +10,52 @@ namespace AdventOfCode2024
         internal static void Problem1()
         {
             long ret = 0;
+            List<long> left = new();
+            List<long> right = new();
 
             foreach (string s in Data.Enumerate())
             {
+                string[] parts = s.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                
+                left.Add(long.Parse(parts[0]));
+                right.Add(long.Parse(parts[1]));
+            }
+
+            left.Sort();
+            right.Sort();
+
+            for (int i = 0; i < left.Count; i++)
+            {
+                ret += long.Abs(left[i] - right[i]);
             }
 
             Console.WriteLine(ret);
         }
 
-        internal static void Problem2x()
+        internal static void Problem2()
         {
+            long ret = 0;
+            List<long> left = new();
+            Dictionary<long, long> right = new();
+
+            foreach (string s in Data.Enumerate())
+            {
+                string[] parts = s.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                left.Add(long.Parse(parts[0]));
+
+                ref long rightSlot = ref CollectionsMarshal.GetValueRefOrAddDefault(right, long.Parse(parts[1]), out _);
+                rightSlot++;
+            }
+
+            left.Sort();
+
+            for (int i = 0; i < left.Count; i++)
+            {
+                ret += left[i] * right.GetValueOrDefault(left[i]);
+            }
+
+            Console.WriteLine(ret);
         }
     }
 }
