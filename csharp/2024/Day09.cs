259c86474af0bd42d89969b3dd1441ccbcf944d9
Jeremy Barton <bartonjs@gmail.com>
2024-12-08 22:14:22 -0800

    Remove some superfluous usings

--- csharp/2024/Day09.cs
+++ csharp/2024/Day09.cs
@@ -2,8 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Runtime.ExceptionServices;
 using AdventOfCode.Util;
 
 namespace AdventOfCode2024
