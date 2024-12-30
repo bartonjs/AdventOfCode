53b32b60b33ef42a5ab68ea3979f8ccf2796150f
Jeremy Barton <bartonjs@gmail.com>
2024-12-29 23:06:11 -0800

    Add a vectorized compare for day 25

--- csharp/2024/Day25.cs
+++ csharp/2024/Day25.cs
@@ -1,27 +1,29 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
+using System.Runtime.Intrinsics;
 using AdventOfCode.Util;
 
 namespace AdventOfCode2024
 {
     internal class Day25
     {
-        private static (List<int[]> Locks, List<int[]> Keys) Load()
+        private static (List<short[]> Locks, List<short[]> Keys) Load()
         {
             List<string> list = new List<string>();
-            List<int[]> locks = new();
-            List<int[]> keys = new();
+            List<short[]> locks = new();
+            List<short[]> keys = new();
 
             foreach (string s in Data.Enumerate().Append(""))
             {
                 if (string.IsNullOrEmpty(s))
                 {
-                    int[] lockOrKey = new int[5];
+                    short[] lockOrKey = new short[Vector<short>.Count];
 
                     if (list[0].IndexOf('.') == -1)
                     {
-                        for (int x = 0; x < lockOrKey.Length; x++)
+                        for (int x = 0; x < 5; x++)
                         {
                             for (int y = 1; y < list.Count; y++)
                             {
@@ -36,7 +38,7 @@ namespace AdventOfCode2024
                     }
                     else
                     {
-                        for (int x = 0; x < lockOrKey.Length; x++)
+                        for (int x = 0; x < 5; x++)
                         {
                             for (int y = list.Count - 2; y >= 0; y--)
                             {
@@ -63,12 +65,12 @@ namespace AdventOfCode2024
 
         internal static void Problem1()
         {
-            (List<int[]> locks, List<int[]> keys) = Load();
+            (List<short[]> locks, List<short[]> keys) = Load();
             long ret = 0;
 
-            foreach (int[] lck in locks)
+            foreach (short[] lck in locks)
             {
-                foreach (int[] key in keys)
+                foreach (short[] key in keys)
                 {
                     bool add = true;
 
@@ -91,8 +93,26 @@ namespace AdventOfCode2024
             Console.WriteLine(ret);
         }
 
-        internal static void Problem2x()
+        internal static void Problem3()
         {
+            (List<short[]> locks, List<short[]> keys) = Load();
+            long ret = 0;
+            Vector128<short> safeLimit = Vector128.Create<short>(5);
+
+            foreach (short[] lck in locks)
+            {
+                foreach (short[] key in keys)
+                {
+                    Vector128<short> sum = Vector128.Create<short>(lck) + Vector128.Create<short>(key);
+
+                    if (Vector128.LessThanOrEqualAll(sum, safeLimit))
+                    {
+                        ret++;
+                    }
+                }
+            }
+
+            Console.WriteLine(ret);
         }
     }
 }
